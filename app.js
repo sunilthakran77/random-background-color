219added7be512e668781494c62d2b929dea84a8
Sunil Thakran <sunilthakran77@gmail.com>
2017-06-15 12:13:30 -0500

    adding javascript code

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 var index = 0, colorValue = "FFFFFF",
 timer = setInterval(function(){startTime()},1000),
 randomColor = ["008000", "FF0000", "A6BEFF", "000000", "99FF99", "FFFF99", "FF00FF", "CCCCCC", "99FFFF", "0000FF"];
